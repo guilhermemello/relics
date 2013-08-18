db8b62e2a1e7a28426e4aa7d3b69dba6b0b72718
Equipe <equipe@simuladosmilitares.com.br>
2013-08-18 13:13:15 -0300

    ajustes nas combos de cidade e bairro (trazer apenas que tenham lojas)

--- app/controllers/combo_controller.rb
+++ app/controllers/combo_controller.rb
@@ -1,11 +1,11 @@
 class ComboController < ApplicationController
   def cidades_por_uf
-    @cidades = Cidade.where("estado_id = ?", params[:estado_id]).all
+    @cidades = Cidade.joins("INNER JOIN lojas ON lojas.cidade_id = cidades.id").where("cidades.estado_id = ?", params[:estado_id]).all
     render :json => @cidades.to_json
   end
 
   def bairros_por_cidade
-    @bairros = Bairro.where("cidade_id = ?", params[:cidade_id]).all
+    @bairros = Bairro.joins("INNER JOIN lojas ON lojas.bairro_id = bairros.id").where("bairros.cidade_id = ?", params[:cidade_id]).all
     render :json => @bairros.to_json
   end
 end
