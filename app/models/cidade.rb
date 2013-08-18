98810b445fb63899d7441a4ab04040ebf9b54c8d
Equipe <equipe@simuladosmilitares.com.br>
2013-08-18 13:21:42 -0300

    ajustes nas combos de cidade e bairro (trazer apenas que tenham lojas)

--- app/models/cidade.rb
+++ app/models/cidade.rb
@@ -7,9 +7,4 @@ class Cidade < ActiveRecord::Base
     cidades = Cidade.where("estado_id = ?", estado_id).all
     cidades.collect { |c| [c.nome, c.id] }
   end
-
-  def self.por_estado_com_loja(estado_id)
-    cidades = Cidade.select("DISTINCT cidades.id, cidades.nome").where("cidades.estado_id = ?", estado_id).joins("INNER JOIN lojas ON lojas.cidade_id = cidades.id").all
-    cidades.collect { |c| [c.nome, c.id] }
-  end
 end
