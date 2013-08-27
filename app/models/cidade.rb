18e02ede22fb7bccd44d950a77a36b37e63a4aa5
Guilherme <guilherme@qcx.com.br>
2013-08-27 09:25:12 -0300

    ajustes na combo de uf do form de lojas

--- app/models/cidade.rb
+++ app/models/cidade.rb
@@ -4,12 +4,12 @@ class Cidade < ActiveRecord::Base
   belongs_to :estado
 
   def self.por_estado(estado_id)
-    cidades = Cidade.where("estado_id = ?", estado_id).all
+    cidades = Cidade.where("estado_id = ?", estado_id).order("nome ASC")
     cidades.collect { |c| [c.nome, c.id] }
   end
 
   def self.por_estado_com_loja(estado_id)
-    cidades = Cidade.select("DISTINCT cidades.id, cidades.nome").where("cidades.estado_id = ?", estado_id).joins("INNER JOIN lojas ON lojas.cidade_id = cidades.id").all
+    cidades = Cidade.select("DISTINCT cidades.id, cidades.nome").where("cidades.estado_id = ?", estado_id).joins("INNER JOIN lojas ON lojas.cidade_id = cidades.id").order("cidades.nome ASC")
     cidades.collect { |c| [c.nome, c.id] }
   end
 end
