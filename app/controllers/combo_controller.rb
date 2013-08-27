f7b7bc12416221de32299fcd53c23b3cb086ed60
Guilherme <guilherme@qcx.com.br>
2013-08-27 09:24:15 -0300

    ajustes na combo de uf do form de lojas

--- app/controllers/combo_controller.rb
+++ app/controllers/combo_controller.rb
@@ -1,21 +1,21 @@
 class ComboController < ApplicationController
   def cidades_por_uf
-    @cidades = Cidade.select("DISTINCT cidades.id, cidades.nome").where("cidades.estado_id = ?", params[:estado_id]).all
+    @cidades = Cidade.select("DISTINCT cidades.id, cidades.nome").where("cidades.estado_id = ?", params[:estado_id]).order("cidades.nome ASC")
     render :json => @cidades.to_json
   end
 
   def cidades_por_uf_com_loja
-    @cidades = Cidade.select("DISTINCT cidades.id, cidades.nome").joins("INNER JOIN lojas ON lojas.cidade_id = cidades.id").where("cidades.estado_id = ?", params[:estado_id]).all
+    @cidades = Cidade.select("DISTINCT cidades.id, cidades.nome").joins("INNER JOIN lojas ON lojas.cidade_id = cidades.id").where("cidades.estado_id = ?", params[:estado_id])..order("cidades.nome ASC")
     render :json => @cidades.to_json
   end
 
   def bairros_por_cidade
-    @bairros = Bairro.select("DISTINCT bairros.id, bairros.nome").where("bairros.cidade_id = ?", params[:cidade_id]).all
+    @bairros = Bairro.select("DISTINCT bairros.id, bairros.nome").where("bairros.cidade_id = ?", params[:cidade_id]).order("bairros.nome ASC")
     render :json => @bairros.to_json
   end
 
   def bairros_por_cidade_com_loja
-    @bairros = Bairro.select("DISTINCT bairros.id, bairros.nome").joins("INNER JOIN lojas ON lojas.bairro_id = bairros.id").where("bairros.cidade_id = ?", params[:cidade_id]).all
+    @bairros = Bairro.select("DISTINCT bairros.id, bairros.nome").joins("INNER JOIN lojas ON lojas.bairro_id = bairros.id").where("bairros.cidade_id = ?", params[:cidade_id]).order("bairros.nome ASC")
     render :json => @bairros.to_json
   end
 end
